9cbd1b567f8c108214ad8485dde3c8e671f47371
kyungtak <kyungtak5310@naver.com>
2020-02-01 10:50:01 +0100

    define search4vowels function

--- venv/python_head_practice.py
+++ venv/python_head_practice.py
@@ -1,4 +1,10 @@
-# TeST
-print("Hello World ! ")
+def search4vowels():
+    vowels = set('aeiou')
+    word = input("Provide a word to search for vowels : ")
+    found = vowels.intersection(set(word))
+    for vowel in found:
+        print(vowel)
 
-# My name is kyungtak
+
+
+search4vowels()
